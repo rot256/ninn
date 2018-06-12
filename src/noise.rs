1679a0be10d6cf20cbcb0fd9ad29c07a3eadc984
Mathias Hall-Andersen <mathias.hall-andersen@nccgroup.com>
2018-06-12 09:41:14 -0400

    Added HKDF tests

--- src/noise.rs
+++ src/noise.rs
@@ -115,3 +115,57 @@ impl State {
         Ok(pt)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    extern crate hex;
+
+    use noise;
+
+    #[test]
+    fn test_hkdf() {
+
+        /* Tests generated using flynn/noise */
+
+        fn decode256(v : &str) -> [u8; 32] {
+            let mut arr = [0; 32];
+            let bytes   = hex::decode(v).unwrap();
+            assert_eq!(bytes.len(), 32);
+            arr.copy_from_slice(&bytes);
+            arr
+        }
+
+        let vectors = [
+            (("c5968636d622340946b99d35202db9c82383f732d99423e2095b4de224244cb9",
+              "2d33c4510cbe6895875c7da8345de88f0b348147ed1d96f7267f6744ad088a17"),
+             ("7dc5bc4197ebc1668dd5e03439827867ba1a667201f1d657629386b59a43cd09",
+              "02a48646f5ee5960f0a2a1e58352b2c5ab5adb3f29769847f0d55de58cbfbe8f",
+              "cb39876304085300bc8bf707888846da8b99a674e0bbba5c13c7b6f42352400c")),
+
+            (("ddddf7ff30cf0d7fdd7d09f94c67778fdd2e3121f8843e59d3f75ce36cf75775",
+              "8490483e7e1213b29602370c6ca99d95ff6c499fc8dad8ccac082ad391b4818d"),
+             ("e0f1b5126ce11ca8f5c604b75381cdc9bbd3e793066dbacc4187fe626ed6d596",
+              "61886f615515f6e3ab3d3145d4708592023c10a5e706e8d72930d9831ee1a15d",
+              "8676c9b5037b937449c550329bbfaf02069a33f1c2d6a1fa5d19ed1ee47ade07")),
+
+            (("0000000000000000000000000000000000000000000000000000000000000000",
+              "0000000000000000000000000000000000000000000000000000000000000000"),
+             ("df7204546f1bee78b85324a7898ca119b387e01386d1aef037781d4a8a036aee",
+              "a7b65a6e7f873068dd147c56493e71294acc89e73baae2e4a87075f18739b4cd",
+              "f0743cc51d27f9b81c0481d34c1e9d42410bda49d6d389387589a364b790742e"))
+        ];
+
+        for vec in vectors.iter() {
+            let (input, output) = vec;
+            let (ck, material)  = input;
+            let (v1, v2, v3)    = output;
+            let (k1, k2, k3)    = noise::hkdf(
+                &decode256(ck),
+                &decode256(material)
+            );
+            assert_eq!(&hex::encode(k1), v1);
+            assert_eq!(&hex::encode(k2), v2);
+            assert_eq!(&hex::encode(k3), v3);
+        }
+    }
+}
